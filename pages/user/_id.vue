0a3cba45c7b63fb7ca17a1686a91a009702bfb3c
endijulian <endijulian080798@gmail.com>
2022-06-30 22:15:05 +0700

    push

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -3,3 +3,11 @@
       <h1>Test Id {{ $route.params.id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+    validate({request}){
+        return /^\d+$/.test(request.params.id);
+    }
+}
+</script>
